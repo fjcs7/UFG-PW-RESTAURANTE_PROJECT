c8e5a0c81e35ede397e5bb9173ca2ab1de530c00
Fernando Junio <fjcs7@hotmail.com>
2016-10-12 21:21:05 -0300

    Criação da tela de login...

---
+++ src/main/webapp/app/login/login.controller.js
@@ -0,0 +1,40 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('appt')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
+    function LoginController($location, AuthenticationService, FlashService) {
+        var vm = this;
+
+        vm.login = login;
+        vm.logado = false;
+
+        (function initController() {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+        })();
+
+        function login() {
+            vm.dataLoading = true;
+            vm.logado = true;
+            AuthenticationService.Login(vm.username, vm.password, function (response) {
+                if (response.success) {
+                    AuthenticationService.SetCredentials(vm.username, vm.password);
+                    if(response.perfil==='garcom'){
+                    	$location.path('/pedidosGarcom');
+                    } else if (response.perfil==='telefonista'){
+                    	$location.path('/pedidosTelefonista');
+                    }
+                } else {
+                    FlashService.Error(response.message);
+                    vm.dataLoading = false;
+                }
+            });
+            
+        };
+    }
+
+})();
