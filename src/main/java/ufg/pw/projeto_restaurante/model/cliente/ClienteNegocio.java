3829904396d2537ccfd2b5c8507aa27e948dd01f
matheussc101 <matheussc101@gmail.com>
2016-12-07 00:43:28 -0200

    Negocio Cliente

---
+++ src/main/java/ufg/pw/projeto_restaurante/model/cliente/ClienteNegocio.java
@@ -0,0 +1,27 @@
+package ufg.pw.projeto_restaurante.model.cliente;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import ufg.pw.projeto_restaurante.model.cliente.dao.ClienteDao;
+
+
+@Path("/clientes")
+public class ClienteNegocio {
+	@GET
+	@Path("/listar_clientes")
+	@Produces("application/json")
+	public List<Cliente> listarClientes(){
+		ClienteDao clienteDao = new ClienteDao();
+		return clienteDao.obterLista();
+	}
+	
+	public void adicionarCliente(Cliente cliente) {
+		ClienteDao clienteDao = new ClienteDao();
+		clienteDao.salvar(cliente);
+	}
+
+}
